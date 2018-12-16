e4e3a2be45224a46be8fbe02956bacb60e2815aa
Josh <joshb2699@gmail.com>
2018-12-16 15:38:40 +0000

    Added bot login

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const Discord = require('discord.js');
+const dotenv = require('dotenv');
+
 const client = new Discord.Client();
+dotenv.config();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.BOT_TOKEN);

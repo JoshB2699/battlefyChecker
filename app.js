0d54988a338eb73b33fdde73854dc34555ab7fd7
Josh <joshb2699@gmail.com>
2018-12-16 15:42:49 +0000

    Added check command skeleton

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
 
+const config = require('./config/config.js');
+
 const client = new Discord.Client();
 dotenv.config();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === config.commandPrefix + 'check') {
+    require('./commands/check.js')(msg);
+  }
 });
 
 client.login(process.env.BOT_TOKEN);

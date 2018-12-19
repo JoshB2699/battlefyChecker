ba52b0c51abe5731d92fd500da86982fd83069c3
Josh <joshb2699@gmail.com>
2018-12-19 15:59:01 +0000

    Added some small Regex testing for the url entered; must now contain 'battlefy.com/teams/' at minimum

--- commands/check.js
+++ commands/check.js
@@ -5,57 +5,63 @@ module.exports = function(msg, config) {
 
   var team_url = msg.content.replace(config.commandPrefix + "check ", "");
 
-  msg.channel.send("Searching for team at: " + team_url)
+  const battlefyRegex = RegExp('battlefy.com/teams/');
+  if(battlefyRegex.test(team_url)){
 
-  puppeteer
-    .launch()
-    .then(function(browser) {
-      return browser.newPage();
-    })
-    .then(function(page) {
-      return page.goto(team_url).then(async function() {
-        await page.waitFor('.ign-text');
-        const renderedContent = await page.evaluate(() => new XMLSerializer().serializeToString(document));
-        return renderedContent;
-      });
-    })
-    .then(async function(html) {
-      var tags = [];
+    msg.channel.send("Searching for team at: " + team_url)
 
-      var tagbytes = await $('div .ign-text', html);
-
-      tagbytes.each(function() {
-        tags.push($(this).text().replace('#', '-'));
+    puppeteer
+      .launch()
+      .then(function(browser) {
+        return browser.newPage();
       })
+      .then(function(page) {
+        return page.goto(team_url).then(async function() {
+          await page.waitFor('.ign-text');
+          const renderedContent = await page.evaluate(() => new XMLSerializer().serializeToString(document));
+          return renderedContent;
+        });
+      })
+      .then(async function(html) {
+        var tags = [];
 
-      return tags;
+        var tagbytes = await $('div .ign-text', html);
 
-    }).then(async function(tags){
-      var sr = [];
-
-      const getProfilePromise = require('util').promisify(overwatch.getProfile);
-
-      async function processTags(tags) {
-        // map array to promises
-        const promises = tags.map(tag => getProfilePromise('pc', 'eu', tag));
-        // wait until all promises are resolved
-        await Promise.all(promises).then(function(profiles){
-          sr = profiles.filter(profile => !isNaN(profile.competitive.rank)).map(profile => profile.competitive.rank)
+        tagbytes.each(function() {
+          tags.push($(this).text().replace('#', '-'));
         })
-      }
 
-      await processTags(tags)
+        return tags;
 
-      sr.sort((a, b) => a - b);
+      }).then(async function(tags){
+        var sr = [];
 
-      var min = Math.min(...sr),
-      max = Math.max(...sr),
-      avg = (sr.reduce((a, b) => a += b)/sr.length).toFixed(0),
-      median =  (sr[(sr.length - 1) >> 1] + sr[sr.length >> 1]) / 2
+        const getProfilePromise = require('util').promisify(overwatch.getProfile);
 
-      msg.channel.send('Mean SR: ' + avg + '\nMedian SR: ' + median + '\nMaximum SR: ' + max + '\nMinimum SR: ' + min + '\nSR Values: ' + sr)
-    })
-    .catch(function(err) {
-      //handle error
-    });
+        async function processTags(tags) {
+          // map array to promises
+          const promises = tags.map(tag => getProfilePromise('pc', 'eu', tag));
+          // wait until all promises are resolved
+          await Promise.all(promises).then(function(profiles){
+            sr = profiles.filter(profile => !isNaN(profile.competitive.rank)).map(profile => profile.competitive.rank)
+          })
+        }
+
+        await processTags(tags)
+
+        sr.sort((a, b) => a - b);
+
+        var min = Math.min(...sr),
+        max = Math.max(...sr),
+        avg = (sr.reduce((a, b) => a += b)/sr.length).toFixed(0),
+        median =  (sr[(sr.length - 1) >> 1] + sr[sr.length >> 1]) / 2
+
+        msg.channel.send('Mean SR: ' + avg + '\nMedian SR: ' + median + '\nMaximum SR: ' + max + '\nMinimum SR: ' + min + '\nSR Values: ' + sr)
+      })
+      .catch(function(err) {
+        //handle error
+      });
+  } else {
+    msg.channel.send('Invalid battlefy team url entered.\nShould be of the form `battlefy.com/teams/id` or `https://battlefy.com/teams/id`')
+  }
 }
